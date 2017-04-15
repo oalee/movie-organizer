f0dda617a0551925d868357ce7001e4f75143a4d
Ali <ilaimihar@gmail.com>
2017-04-15 21:26:51 +0430

    updated the size of folder function

--- fileTools.py
+++ fileTools.py
@@ -6,13 +6,21 @@ from pathlib import Path
 def du(path):
     """disk usage in human readable format (e.g. '2,1GB')"""
     return subprocess.check_output(['du','-sh', path]).split()[0].decode('utf-8')
-
+    
+    
+def get_size(start_path = '.'):
+    total_size = 0
+    for dirpath, dirnames, filenames in os.walk(start_path):
+        for f in filenames:
+            fp = os.path.join(dirpath, f)
+            total_size += os.path.getsize(fp)
+    return total_size
     
 def getMovieList(dirs):
     movies_list = []
     for dir in dirs:
         if Path(dir).is_dir():
-            movies_list += [(str(x).split("/")[-1] , str(x) , du(str(x))) for x in Path(dir).iterdir() if x.is_dir() ]
+            movies_list += [(str(x).split("/")[-1] , str(x) , get_size(str(x) )) for x in Path(dir).iterdir() if x.is_dir() ]
             #Movie  Name , Location and size
     return movies_list
     
@@ -20,7 +28,7 @@ def getAllSubdirectories(dirs):
     movies_list = []
     for dir in dirs:
         if Path(dir).is_dir():
-            movies_list += [(str(x).split("/")[-1] , str(x) , du(str(x))) for x in Path(dir).iterdir() ]
+            movies_list += [(str(x).split("/")[-1] , str(x) , get_size(str(x))) for x in Path(dir).iterdir() ]
             #Movie  Name , Location and size
     return movies_list
     
